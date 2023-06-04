3c3b28dab940120ccc796429f093cf1d906bf846
Joshua Angona <joshuaangona@icloud.com>
2023-06-04 18:26:16 -0400

    4565

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const path = require('path');
-const { pathToFileURL } = require('url');
 const app = express();
 
 // Enable CORS
 app.use(cors());
-app.use(express.text());
+app.use(express.json());
+
+// Serve the text file as a static asset
+app.use('/data', express.static('data.txt'));
 
 app.post('/api/submit', (req, res) => {
-    // Access form data from req.body and process/store it
-    // Example: const formData = req.body;
-    // Store the data in your desired storage mechanism
     const finalString = req.body;
 
-    // Send a response or send a success status
-    console.log(req.body); // Check the received data
-    fs.writeFile('/data.txt', finalString, 'utf8', (err) => {
+    // Store the data in a text file
+    fs.appendFile('data.txt', finalString + '\n', (err) => {
         if (err) {
             console.error('Error writing to file:', err);
             res.sendStatus(500);
         } else {
-            console.log('Data stored in submitted_data.txt');
-            console.log(pathToFileURL("data.txt"));
+            console.log('Data written to file successfully');
             res.sendStatus(200);
         }
     });
