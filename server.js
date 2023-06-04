9c54730d4d2f0bd6715202b397b6442ed99587c8
Joshua Angona <joshuaangona@icloud.com>
2023-06-04 17:59:36 -0400

    edd

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 
 // Enable CORS
 app.use(cors());
-app.use(express.text());
+app.use(express.json());
 
 app.post('/api/submit', (req, res) => {
-    // Access form data from req.body and process/store it
-    // Example: const formData = req.body;
-    // Store the data in your desired storage mechanism
     const finalString = req.body;
 
-    // Send a response or send a success status
-    console.log(req.body); // Check the received data
+    // Store the data in a text file
+    fs.appendFile('data.txt', finalString + '\n', (err) => {
+        if (err) {
+            console.error('An error occurred while storing the data:', err);
+            return res.status(500).json({ message: 'Error storing the data' });
+        }
 
-    res.send("Submitted Scouting Form");
+        console.log('Data stored successfully');
+        return res.sendStatus(200);
+    });
 });
 
 app.listen(process.env.PORT || 8000, () => {
